fe590595ec73204390285531b5efa2691e42c0de
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-09-28 17:07:42 +0300

    ..

--- src/app/components/authLinks/AuthLinks.jsx
+++ src/app/components/authLinks/AuthLinks.jsx
@@ -1,18 +1,44 @@
+"use client"
 import styles from "./authLinks.module.css"
 import Link from "next/link"
+import { useState } from "react"
 
 export default function AuthLinks() {
   const status = "notauthenticated"
+  const [open, setOpen] = useState(false)
   return (
     <>
       {status === "notauthenticated" ? (
-        <Link href="/login">Login</Link>
+        <Link href="/login" className={styles.link}>Login</Link>
       ) : (
         <>
-          <Link href="/write">Write
+          <Link href="/write" className={styles.link}>Write
           </Link>
-          <span>Logout</span>
+          <span className={styles.link}>Logout</span>
         </>
+      )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+          {
+            status === "notauthenticated" ? (
+              <Link href="/login">Login</Link>
+            ) : (
+              <>
+                <Link href="/write">Write
+                </Link>
+                <span className={styles.link}>Logout</span>
+              </>
+            )
+          }
+        </div>
       )}
     </>
   )
--- src/app/components/featured/Featured.jsx
+++ src/app/components/featured/Featured.jsx
@@ -1,7 +1,20 @@
 import styles from "./featured.module.css"
+import Image from "next/image"
 
 export default function Featured() {
   return (
-    <div className={styles.container}>Featured</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}><b>It's Me, Mohammed Khaled </b>Discover All My great Ideas and Stories</h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="hero" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nulla.</h1>
+          <p className={styles.postDesc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa tenetur recusandae nihil nostrum reprehenderit earum non placeat accusamus similique velit consectetur quis quam eaque quasi, in exercitationem voluptates sint! Recusandae!</p>
+          <button>Read More</button>
+        </div>
+      </div>
+    </div>
   )
 }
