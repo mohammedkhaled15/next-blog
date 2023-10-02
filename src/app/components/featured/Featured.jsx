35136cb03e679df5ff1115d9df756b10f7ca869c
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-10-02 16:25:01 +0300

    ..

--- src/app/components/featured/Featured.jsx
+++ src/app/components/featured/Featured.jsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function Featured() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}><b>It's Me, Mohammed Khaled </b>Discover All My great Ideas and Stories</h1>
+      <h1 className={styles.title}><b style={{ color: "white" }}>It&apos;s Me, Mohammed Khaled </b>Discover All My Great Ideas and Stories</h1>
       <div className={styles.post}>
         <div className={styles.imgContainer}>
           <Image src="/p1.jpeg" alt="hero" fill />
@@ -12,7 +12,7 @@ export default function Featured() {
         <div className={styles.textContainer}>
           <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nulla.</h1>
           <p className={styles.postDesc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa tenetur recusandae nihil nostrum reprehenderit earum non placeat accusamus similique velit consectetur quis quam eaque quasi, in exercitationem voluptates sint! Recusandae!</p>
-          <button>Read More</button>
+          <button className={styles.button}>Read More</button>
         </div>
       </div>
     </div>
