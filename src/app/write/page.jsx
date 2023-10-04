6e0cc2183606a752773c1c10033937fa4d329b7f
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-10-04 14:05:50 +0300

    ..

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -12,28 +12,29 @@ export default function WritePage() {
 
   return (
     <div className={styles.container}>
-      <input type="text" placeholder="Title" />
+      <input type="text" placeholder="Title" className={styles.input} />
       <div className={styles.editor}>
-        <button className={styles.button}>
+        <button className={styles.button} onClick={() => setOpen(!open)}>
           <Image src="/plus.png" alt="" width={16} height={16} />
         </button>
         {
           open && (
             <div className={styles.add}>
               <button className={styles.addButton}>
-                <Image src="/plus.png" alt="" width={16} height={16} />
+                <Image src="/image.png" alt="" width={16} height={16} />
               </button>
               <button className={styles.addButton}>
-                <Image src="/plus.png" alt="" width={16} height={16} />
+                <Image src="/external.png" alt="" width={16} height={16} />
               </button>
               <button className={styles.addButton}>
-                <Image src="/plus.png" alt="" width={16} height={16} />
+                <Image src="/video.png" alt="" width={16} height={16} />
               </button>
             </div>
           )
         }
-        <ReactQuill theme="bubble" value={value} onChange={setValue} placeholder="Tell Your Story" />
+        <ReactQuill className={styles.textArea} theme="bubble" value={value} onChange={setValue} placeholder="Tell Your Story" />
       </div>
+      <button className={styles.publish}>Publish</button>
     </div>
   )
 }
