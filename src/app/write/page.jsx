cfbc8d70174a3866f6ad4db89897746e13ee3e82
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-10-11 13:35:10 +0300

    ..

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -6,6 +6,9 @@ import "react-quill/dist/quill.bubble.css"
 import { useSession } from "next-auth/react"
 import { usePathname, useRouter } from "next/navigation"
 import dynamic from "next/dynamic"
+import CreatableSelect from 'react-select/creatable';
+import makeAnimated from 'react-select/animated';
+import useSWR from "swr"
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { app } from "@/utils/firebase"
 
@@ -22,9 +25,28 @@ export default function WritePage() {
   const [media, setMedia] = useState("")
   const [title, setTitle] = useState("")
 
+  const [selectedOptions, setSelectedOptions] = useState([])
+  const [selectOptions, setSelectOptions] = useState([])
+  const [newCats, setNewCats] = useState([])
+
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  const { data: cats, mutate, isLoading } = useSWR(`/api/comments`, fetcher)
+
   const { status } = useSession()
   const router = useRouter()
   const pathName = usePathname()
+
+  const handleSelectChange = (options, actionMeta) => {
+    if (actionMeta.action === "create-option") {
+      // when action is create we will create new skills array to update the db with it
+      setNewCats((prev) => [...prev, actionMeta.option])
+    }
+    setSelectedOptions(options)
+  }
+
+  useEffect(() => {
+    skills && setSelectOptions(cats)
+  }, [cats])
 
   useEffect(() => {
     const uploadFile = () => {
@@ -87,11 +109,34 @@ export default function WritePage() {
     router.push(`/login?from=${pathName}`)
   }
 
-
+  const animatedComponents = makeAnimated();// for animation of selected labels in select input "from docs"
 
   return (
     <div className={styles.container}>
       <input type="text" placeholder="Title" className={styles.input} onChange={(e) => setTitle(e.target.value)} />
+      <div className={styles.select}>
+        <CreatableSelect
+          menuPortalTarget={window.document.body}
+          id="cats"
+          styles={{
+            container: (base, state) => ({ ...base, width: "100%", minHeight: "30px", border: "none" }),
+            control: (base) => ({ ...base, minHeight: "25px", backgroundColor: "transparent", color: "white" }),
+            input: (base) => ({ ...base, color: "white" }),
+            menu: (base) => ({ ...base, borderRadius: "10px" }),
+            menuList: (base) => ({ ...base, backgroundColor: "#1F1F38", overflow: "scroll" }),
+            option: (base, state) => ({ ...base, backgroundColor: state.isFocused ? "#4DB5FF" : "", fontSize: "12px" }),
+            multiValueLabel: (base) => ({ ...base, backgroundColor: "#4DB5FF", color: "white", padding: "3px", width: "fit-content" }),
+          }}
+          options={selectOptions}
+          components={animatedComponents}
+          isMulti={true}
+          closeMenuOnSelect
+          onChange={handleSelectChange}
+          placeholder="Select Or Create new Category..."
+          value={selectedOptions}
+        />
+        <label htmlFor="cats">Select Categories</label>
+      </div>
       <div className={styles.editor}>
         <button className={styles.button} onClick={() => setOpen(!open)}>
           <Image src="/plus.png" alt="" width={16} height={16} />
