5fbee708b38729d9bc64e202d4f094fd7884bcb4
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-10-12 15:09:40 +0300

    ..

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -87,7 +87,7 @@ export default function WritePage() {
   const slugify = str =>
     str.toLowerCase()
       .trim()
-      .replace(/[^\w\s-]/g, "")
+      // .replace(/[^\w\s-]/g, "")
       .replace(/[\s_-]+/g, "-")
       .replace(/^-+|-+$]+/g, "")
 
