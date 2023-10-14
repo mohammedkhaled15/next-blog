e0dc749125a691b2976c514068626e3b4ae7565b
mohammedkhaled15 <52936327+mohammedkhaled15@users.noreply.github.com>
2023-10-14 21:39:07 +0300

    Update connectDb.js

--- src/utils/connectDb.js
+++ src/utils/connectDb.js
@@ -3,10 +3,10 @@ let prisma;
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!globalThis.prisma) {
-    globalThis.prisma = new PrismaClient();
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
   }
-  prisma = globalThis.prisma;
+  prisma = global.prisma;
 }
 
 export default prisma;
